61e1ca23a6702882c5b89a909a99ff0bf8b5a77b
Halidha Aarif <96538764+Halidha99@users.noreply.github.com>
2025-11-01 13:51:16 +0530

    Develop Admin Dsashboard

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,17 +1,16 @@
-<x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Admin Dashboard') }}
-        </h2>
-    </x-slot>
+@extends('admin.maindesign')
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
-                    {{ __("You're logged in!") }}
-                </div>
+@section('title', 'Admin Dashboard')
+
+@section('page_title', 'Admin Dashboard')
+
+@section('content')
+    <div class="container-fluid">
+        <div class="card">
+            <div class="card-body">
+                <h4 class="card-title">Welcome, Admin!</h4>
+                <p>{{ __("You're logged in!") }}</p>
             </div>
         </div>
     </div>
-</x-app-layout>
+@endsection
